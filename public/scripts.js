ecb715fc2dbbdf04629bca66e63aea5558e3da08
Katherine Vo <katherinevo09@gmail.com>
2021-03-26 18:34:27 -0400

    updated js to append dining hall data to the html

--- public/scripts.js
+++ public/scripts.js
@@ -1,6 +1,22 @@
 async function windowActions() {
   const request = await fetch('/api/dining');
-  const data = await request.json();
+  const data = await request.json()
+  // console.log(data["data"])
+  datas = data["data"];
+  const hall = document.querySelector('.hall');
+
+  datas.forEach((item) => {
+
+    const appendItem = document.createElement('tr');
+
+    appendItem.innerHTML = `
+      <td class="id-num">${item.hall_id}</td>
+      <td>${item.hall_name}</td>
+      <td>${item.hall_address}</td>`;
+
+    hall.append(appendItem);
+  });
+
 }
 
 window.onload = windowActions;
