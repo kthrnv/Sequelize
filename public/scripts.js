9b26267f7e649b309a568a0a4323a89db1a7257c
Katherine Vo <katherinevo09@gmail.com>
2021-04-09 00:49:15 -0400

    picking 10 random meals and its macros

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,9 @@
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+
 async function getMeals() {
   const diningRequest = await fetch('/api/wholeMeal');
   const diningData = await diningRequest.json();
@@ -10,8 +16,16 @@ async function windowActions() {
   // console.log(data["data"])
   datas = data["data"];
   const hall = document.querySelector('.hall');
-  const meals = await getMeals();
-  console.table(meals.data);
+  const results = await getMeals();
+  const meals = results.data;
+  console.table(meals);
+
+  const mealArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const selectedMeals = mealArray.map(element => {
+    const random = getRandomIntInclusive(0, meals.length - 1);
+    return meals[random];
+  });
+  console.table(selectedMeals);
 
   datas.forEach((item) => {
 
